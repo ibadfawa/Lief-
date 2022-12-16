32cbaef2159db91e41b7f8fb58896ea8a647a78a
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 13:48:14 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,5 +1,5 @@
 import lief
 
 libnative = lief.parse("libjiagu_a64.so") #nama lib yang ingin di inject
-libnative.add_library("libgadget.so") # gadget lu
+libnative.add(self: lief.ELF.Segment, flag: lief.ELF.SEGMENT_FLAGS)
 libnative.write("libjiagu_a64new.so")
