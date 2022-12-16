3e7891bb66aefd7bcbfec95ca24389035978205a
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 13:07:27 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,7 +1,23 @@
+
 import lief
 
- elf = lief.parse("libjiagu_a64.so")
-  for entry in elf.dynamic_entries:
-    if entry.tag == lief.ELF.DYNAMIC_TAGS.INIT:
-      entry.value = 0x88888888
- elf.write("test_modified")
+elf: lief.ELF.Binary = lief.parse("libjiagu_a64.so")
+
+# Force relocating the .dynamic/.dynstr
+elf.add_library("libibad.so")
+
+# For relocating the interpreter
+#elf.interpreter = "/a/very/longlonglong/interpreter-1.2.3.bin"
+
+# Force relocating .dynsym / .gnu.hash table
+for i in range(10):
+    elf.add_exported_function(0xdeadc0de + i, f"new_export_{i}")
+
+# Add a segment
+segment         = lief.ELF.Segment()
+segment.type    = lief.ELF.SEGMENT_TYPES.LOAD
+segment.content = [0xcc] * 0x23
+
+elf.add(segment)
+
+elf.write("jiagu1.so")
