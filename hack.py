f0f832f0ec207705fe5ea575f52a710867ba6460
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 13:55:42 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,22 +1,5 @@
 import lief
-def main():
-    logging.basicConfig()
-    logger = logging.getLogger(os.path.basename(__file__))
 
-    elffile = lief.parse("libjiagu_a64.so")
-
-   
-    
-
-    new_sec = lief.ELF.Section()
-    new_sec.name = ".ext_bin"
-    
-    new_sec.alignment = 4
-    new_sec.virtual_address = 100000
-
-    elffile.add(new_sec, True)
-    print ('size written : %s' % new_sec.size, 'size expected: %s' )
-
-    elffile.write("out.elf")
-
-    
+libnative = lief.parse("libjiagu_a64.so") #nama lib yang ingin di inject
+libnative.add(lief.ELF.Segment, lief.None)
+libnative.write("out.so")
