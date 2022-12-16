c2ce8bd836865a1f93ddfc34d349ba8f4417a945
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 12:58:44 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,5 +1,7 @@
-  elf = lief.parse("libjiagu_a64.so")
+import lief
+
+ elf = lief.parse("libjiagu_a64.so")
   for entry in elf.dynamic_entries:
     if entry.tag == lief.ELF.DYNAMIC_TAGS.INIT:
       entry.value = 0x88888888
-  elf.write("test_modified")
+ Elf.write("test_modified")
