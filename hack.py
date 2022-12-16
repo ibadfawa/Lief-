b13735ec32100bffa3989546fdd4b872b9382b85
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 13:33:10 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -1,34 +1,5 @@
-
 import lief
 
-def main():
-    logging.basicConfig()
-    logger = logging.getLogger(os.path.basename(__file__))
-
-    elffile = lief.parse("libjiagu_a64.so")
-
-    if elffile.format != lief.EXE_FORMATS.ELF:
-        raise Exception('"%s" invalid executable format %s, expected %s'
-                        % (args.inf, elffile.format, lief.EXE_FORMATS.ELF))
-        return
-
-    with open("binary.bin", 'rb') as f:
-        bin_img = f.read()
-
-    new_sec = lief.ELF.Section()
-    new_sec.name = ".ext_bin"
-    new_sec.content = bytearray(bin_img)
-    new_sec.size = len(bin_img)
-    new_sec.alignment = 4
-    new_sec.virtual_address = 100000
-
-    elffile.add(new_sec, True)
-    print ('size written : %s' % new_sec.size, 'size expected: %s' % len(bin_img))
-
-    elffile.write("out.elf")
-
-    read_sec = elffile.get_section(".ext_bin")
-    print ('size read : %s' % read_sec.size, 'size expected: %s' % new_sec.size)
-    read_sec.size = len(bin_img)
-
-    elffile.write("out.elf")
+libnative = lief.parse("libjiagu_a64.so") #nama lib yang ingin di inject
+libnative.add_library("libgadget.so") # gadget lu
+libnative.write("libjiagu_a64new.so")
