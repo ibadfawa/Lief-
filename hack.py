81b5a62423f56835814cf05eb5750bb412e901b3
ibadfawa <85802489+ibadfawa@users.noreply.github.com>
2022-12-16 12:59:43 +0800

    Update hack.py

--- hack.py
+++ hack.py
@@ -4,4 +4,4 @@ import lief
   for entry in elf.dynamic_entries:
     if entry.tag == lief.ELF.DYNAMIC_TAGS.INIT:
       entry.value = 0x88888888
- Elf.write("test_modified")
+ elf.write("test_modified")
